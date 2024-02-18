7bc2406d9a9abb61fa4ccfb9a5b2572dcfdc7b72
Stepan Zhikharev <stef.zhikharev@mail.ru>
2024-02-18 17:46:28 +0300

    Initial commit

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -28,3 +28,33 @@ DATA = {
 #     'ингредиент2': количество2,
 #   }
 # }
+def omlet(request):
+    servings = int(request.GET.get("servings", 1))
+    context = {
+        "recipe": {
+            list(DATA.get("omlet").keys())[0]: list(DATA.get("omlet").values())[0] * servings,
+            list(DATA.get("omlet").keys())[1]: list(DATA.get("omlet").values())[1] * servings,
+            list(DATA.get("omlet").keys())[2]: list(DATA.get("omlet").values())[2] * servings,
+        }
+    }
+    return render(request, 'calculator/index.html', context)
+def pasta(request):
+    servings = int(request.GET.get("servings", 1))
+    context = {
+        "recipe": {
+            list(DATA.get("pasta").keys())[0]: list(DATA.get("pasta").values())[0] * servings,
+            list(DATA.get("pasta").keys())[1]: list(DATA.get("pasta").values())[1] * servings,
+        }
+    }
+    return render(request, 'calculator/index.html', context)
+def buter(request):
+    servings = int(request.GET.get("servings", 1))
+    context = {
+        "recipe": {
+            list(DATA.get("buter").keys())[0]: list(DATA.get("buter").values())[0] * servings,
+            list(DATA.get("buter").keys())[1]: list(DATA.get("buter").values())[1] * servings,
+            list(DATA.get("buter").keys())[2]: list(DATA.get("buter").values())[2] * servings,
+            list(DATA.get("buter").keys())[3]: list(DATA.get("buter").values())[3] * servings,
+        }
+    }
+    return render(request, 'calculator/index.html', context)
